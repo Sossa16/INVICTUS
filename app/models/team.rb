16e55371fabbeb4f4a399d3f1d37bf6788d63029
Santiago <santiago_1016@hotmail.com>
2017-08-02 22:35:44 -0500

    imagenes de fondo

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,6 @@ class Team < ApplicationRecord
   	validates :team_name, presence: true, length: { minimum: 5 }
 	validates :description, presence: true
 
-	has_attached_file :cover, style: { medium: "800x720", thumb: "800x600"}
+	has_attached_file :cover, style: { medium: "800x720", thumb: "300x300"}
 	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
 end
